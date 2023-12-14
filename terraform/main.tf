752c9f38f3c0f5cef296f85f451c1f849e428ea4
pavankumar0077 <dasarepavan007@gmail.com>
2023-12-14 12:10:23 +0000

    test1212

---
+++ terraform/main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example_instance" {
+  ami           = "ami-0fc5d935ebf8bc3bc"  # Specify the AMI ID for your desired image
+  instance_type = "t2.micro"
+  
+  key_name = "pavan"  # Specify the name of your key pair
+
+  tags = {
+    Name    = "example-instance"
+    Region  = "us-east-1"
+  }
+
+  // Define security group to allow incoming traffic on ports 22 and 8000
+  vpc_security_group_ids = ["${aws_security_group.example_sg.id}"]
+}
+
+# Create a security group allowing incoming traffic on ports 22 and 8000
+resource "aws_security_group" "example_sg" {
+  name        = "example-sg"
+  description = "Allow incoming traffic on ports 22 and 8000"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Output the public IP address of the created instance
+output "public_ip" {
+  value = aws_instance.example_instance.public_ip
+}
+
