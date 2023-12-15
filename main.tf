5ad8e1e958df5c4b23e5d16c9390e46e999b5753
pavankumar0077 <dasarepavan007@gmail.com>
2023-12-15 11:10:15 +0000

    sljdf

--- main.tf
+++ main.tf
@@ -2,117 +2,82 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Existing VPC details
-variable "existing_vpc_id" {
-  description = "ID of the existing VPC"
-  default     = "vpc-0c144991f30bbb9ab"
+data "aws_vpc" "existing" {
+  id = "vpc-0c144991f30bbb9ab" 
 }
 
-# Existing Internet Gateway details
-variable "existing_igw_id" {
-  description = "ID of the existing Internet Gateway"
-  default     = "igw-0a1671ae326a4b5e1"
+data "aws_subnet" "public" {
+  id = "subnet-08149cb0fe0b5dfb8"
 }
 
-# Existing key pair name
-variable "key_pair_name" {
-  description = "Name of the existing key pair"
-  default     = "pavan.pem"
+data "aws_internet_gateway" "gw" {
+  id = "igw-0a1671ae326a4b5e1"
 }
 
-# Create a public subnet in the existing VPC
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = var.existing_vpc_id
-  cidr_block              = "10.0.1.0/24"  # Choose an appropriate CIDR block for your public subnet
-  availability_zone       = "us-east-1a"    # Choose your desired availability zone
-
-  map_public_ip_on_launch = true  # This will automatically assign public IPs to instances in this subnet
-
-  tags = {
-    Name = "public-subnet"
-  }
+data "aws_route_table" "public" {
+  id = "rtb-0fd149856f8a84298"  
 }
 
-# Create an Internet Gateway
-resource "aws_internet_gateway" "example_igw" {
-  vpc_id = var.existing_vpc_id
-
-  tags = {
-    Name = "example-igw"
-  }
+resource "aws_route_table_association" "public" {
+  subnet_id      = data.aws_subnet.public.id
+  route_table_id = data.aws_route_table.public.id
 }
 
-# Create a route table for the public subnet
-resource "aws_route_table" "public_route_table" {
-  vpc_id = var.existing_vpc_id
+resource "aws_security_group" "web" {
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.example_igw.id
-  }
-
-  tags = {
-    Name = "public-route-table"
-  }
-}
-
-# Associate the public subnet with the public route table
-resource "aws_route_table_association" "public_subnet_association" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_route_table.id
-}
-
-# Create a security group allowing inbound traffic on ports 22, 80, 443, and 8000
-resource "aws_security_group" "example_sg" {
-  name        = "example-sg"
-  description = "Allow inbound traffic on ports 22, 80, 443, and 8000"
+  vpc_id = data.aws_vpc.existing.id
 
   ingress {
-    from_port   = 22  # SSH
-    to_port     = 22
+    from_port   = 22
+    to_port     = 22 
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 80  # HTTP
+    from_port   = 80 
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] 
   }
 
   ingress {
-    from_port   = 443  # HTTPS
-    to_port     = 443
+    from_port   = 443
+    to_port     = 443  
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] 
   }
 
   ingress {
-    from_port   = 8000  # Custom application port
+    from_port   = 8000     
     to_port     = 8000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port       = 0
+    to_port         = 0 
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
 }
 
-# Create an EC2 instance in the public subnet
-resource "aws_instance" "example_instance" {
-  ami           = "ami-0fc5d935ebf8bc3bc"  # Specify the AMI ID for your desired image
+resource "aws_instance" "web" {
+
+  ami           = "ami-0fc5d935ebf8bc3bc"
   instance_type = "t2.micro"
+
+  key_name = "pavan.pem"  # Update this line with your key pair name
+
+  subnet_id                   = data.aws_subnet.public.id 
+  vpc_security_group_ids      = [aws_security_group.web.id]
   
-  key_name      = var.key_pair_name
-  subnet_id     = aws_subnet.public_subnet.id
-  vpc_security_group_ids = [aws_security_group.example_sg.id]
+  associate_public_ip_address = true
 
   tags = {
-    Name    = "example-instance"
-    Region  = "us-east-1"
+    Name = "Web Server"
   }
-}
-
-# Output the public IP address of the created instance
-output "public_ip" {
-  value = aws_instance.example_instance.public_ip
 }
 
