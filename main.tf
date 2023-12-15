5e517e916e911e18e37abeee18bd8baa4fb9fbd1
pavankumar0077 <dasarepavan007@gmail.com>
2023-12-15 12:05:10 +0000

    sdf

--- main.tf
+++ main.tf
@@ -2,25 +2,43 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example_instance" {
-  ami           = "ami-0fc5d935ebf8bc3bc"  # Specify the AMI ID for your desired image
-  instance_type = "t2.micro"
-  
-  key_name = "pavan"  # Specify the name of your key pair
+# Create a new VPC
+resource "aws_vpc" "example_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
 
   tags = {
-    Name    = "example-instance"
-    Region  = "us-east-1"
+    Name = "example-vpc"
   }
+}
 
-  // Define security group to allow inbound traffic on ports 22, 80, 443, and 8000
-  vpc_security_group_ids = ["${aws_security_group.example_sg1.id}"]
+# Create a new subnet in the VPC
+resource "aws_subnet" "example_subnet" {
+  vpc_id                  = aws_vpc.example_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "example-subnet"
+  }
+}
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "example_igw" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  tags = {
+    Name = "example-igw"
+  }
 }
 
 # Create a security group allowing inbound traffic on ports 22, 80, 443, and 8000
 resource "aws_security_group" "example_sg1" {
   name        = "example-sg1"
   description = "Allow inbound traffic on ports 22, 80, 443, and 8000"
+  vpc_id      = aws_vpc.example_vpc.id
 
   ingress {
     from_port   = 22  # SSH
@@ -56,6 +74,40 @@ resource "aws_security_group" "example_sg1" {
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create a route table
+resource "aws_route_table" "example_rt" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example_igw.id
+  }
+
+  tags = {
+    Name = "example-route-table"
+  }
+}
+
+# Associate the subnet with the route table
+resource "aws_route_table_association" "example_rt_a" {
+  subnet_id      = aws_subnet.example_subnet.id
+  route_table_id = aws_route_table.example_rt.id
+}
+
+# Create an EC2 instance in the new subnet with the created security group
+resource "aws_instance" "example_instance" {
+  ami           = "ami-0fc5d935ebf8bc3bc"  # Specify the AMI ID for your desired image
+  instance_type = "t2.micro"
+  key_name      = "pavan"  # Specify the name of your key pair
+  subnet_id     = aws_subnet.example_subnet.id
+  vpc_security_group_ids = [aws_security_group.example_sg1.id]
+
+  tags = {
+    Name   = "example-instance"
+    Region = "us-east-1"
   }
 }
 
